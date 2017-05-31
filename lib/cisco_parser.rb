d0e410a9c8598e17a8d271ce5310b2e61f28a7c8
Roman Priesol <roman@priesol.net>
2017-05-31 23:56:43 +0200

    cd /home/roman.priesol/Projects/ruby/cisco_parser/lib

--- lib/cisco_parser.rb
+++ lib/cisco_parser.rb
@@ -2,3 +2,4 @@ require "cisco_parser/version"
 
 require "cisco_parser/ios"
 require "cisco_parser/wlc"
+require "cisco_parser/aossw"
--- lib/cisco_parser/version.rb
+++ lib/cisco_parser/version.rb
@@ -1,3 +1,3 @@
 module CiscoParser
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
